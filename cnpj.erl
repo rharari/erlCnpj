ac46a4e4fef1107bbe687a09c2fa51809ca5fc43
Ricardo Harari <ricardo.harari@gmail.com>
2015-02-17 13:54:58 -0200

    Update cnpj.erl

--- cnpj.erl
+++ cnpj.erl
@@ -12,8 +12,8 @@
 isValid(S) ->
 	C = normalizar(lists:filter(fun(X) -> X>47 andalso X<58 end, S)),
 	D = lists:sum( lists:zipwith(fun(X,Y) -> (X-48)*Y end, C, [6,7,8,9,2,3,4,5,6,7,8,9,0,0]) ) rem 11,
-	D == lists:nth(13, C) - 48 andalso 
-		( lists:sum(lists:zipwith(fun(X,Y) -> (X-48)*Y end, C, [5,6,7,8,9,2,3,4,5,6,7,8,0,0]) ) + D * 9 ) rem 11 == lists:nth(14, C) - 48.
+	D =:= lists:nth(13, C) - 48 andalso 
+		( lists:sum(lists:zipwith(fun(X,Y) -> (X-48)*Y end, C, [5,6,7,8,9,2,3,4,5,6,7,8,0,0]) ) + D * 9 ) rem 11 =:= lists:nth(14, C) - 48.
 
 %% Internal functions
 %% ====================================================================
